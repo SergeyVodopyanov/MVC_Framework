8fe8b4c22001d2c7988a4a7b06ed956f2afe260d
SergeyVodopyanov <vodopyanov03@bk.ru>
2024-04-10 19:02:01 +0300

    Виды и шаблоны, класс View

---
+++ application/views/account/login.php
@@ -0,0 +1,8 @@
+<h3>Авторизация</h3>
+<form> 
+    <p>Логин</p>
+    <p><input type="text"></p>
+    <p>Пароль</p>
+    <p><input type="text"></p>
+    <b><button>Авторизоваться</button></b>
+</form>
